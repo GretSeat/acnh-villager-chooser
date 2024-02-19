5db93df9b3c6e5f838ffe1b12b81f5ef75f75390
Garrett Stewart <garrettbstewart@gmail.com>
2024-02-19 11:40:15 -0500

    added some components

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <p>This is the Main Content</p>
+    </main>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>This is an Option</li>
+        <li>This is another Option</li>
+      </ul>
+    </nav>
+  );
+}
