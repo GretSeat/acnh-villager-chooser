b7e9d6fab55e5dafb69dc939d573c3b79133bf54
Garrett Stewart <garrettbstewart@gmail.com>
2024-02-28 11:11:49 -0500

    Add Drops Downs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,30 @@ export default function Main() {
   return (
     <main>
       <p>This is the Main Content</p>
+      {/* Gender */}
+      <select name="" id="">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+      {/* Species */}
+      <select name="" id="">
+        <option value="all">All</option>
+      </select>
+      {/* Personality Type */}
+      <select name="" id="">
+        <option value="Snooty">Snooty</option>
+        <option value="Lazy">Lazy</option>
+        <option value="Peppy">Peppy</option>
+      </select>
+      {/* Hobby*/}
+      <select name="" id="">
+        <option value="all">All</option>
+      </select>
+      {/* Birthday Month */}
+      <select name="" id="">
+        <option value="all">All</option>
+      </select>
+      {/*  */}
     </main>
   );
 }
