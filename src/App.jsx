db91b7ddf66e048c8e27928b25af1f39b5165d97
Garrett Stewart <garrettbstewart@gmail.com>
2024-03-15 12:55:46 -0400

    added useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
-import Home from "./pages/Home";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [acData, setAcData] = useState({});
+  const [count, setCount] = useState(0);
+
+  useEffect(
+    function () {
+      fetch("")
+        .then((res) => res.json())
+        .then((data) => setAcData(data));
+      console.log("Data Loaded");
+    },
+    [count]
+  );
+
   return (
     <>
-      <Home />
+      <pre>{JSON.stringify(acData, null, 2)}</pre>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Add
+      </button>
     </>
   );
 }
